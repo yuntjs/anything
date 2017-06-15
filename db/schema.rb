210587ff31b313c198867fe5627894e3985d713c
Tae Yun <yuntjs@gmail.com>
2017-06-15 15:56:40 -0400

    fix old issues

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,6 @@ ActiveRecord::Schema.define(version: 20161227061513) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "yelp_id"
-    t.string   "name"
-    t.string   "image_url"
-    t.string   "location"
     t.string   "yelp_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
